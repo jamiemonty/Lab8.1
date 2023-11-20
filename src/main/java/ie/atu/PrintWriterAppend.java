a45b74b03e3c6bf9add01337d399fb2b8ec8163e
G00419525@atu.ie <G00419525@atu.ie>
2023-11-20 17:33:48 +0000

    Printwriter Append

--- src/main/java/ie/atu/PrintWriterAppend.java
+++ src/main/java/ie/atu/PrintWriterAppend.java
@@ -1,4 +1,22 @@
 package ie.atu;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class PrintWriterAppend {
+    public static void main(String[] args) {
+
+        String fileName = "file12.txt";
+
+        try(PrintWriter writer = new PrintWriter(new FileWriter(fileName, true)))
+        {
+            writer.println("This line will be appended");
+
+        }
+        } catch (IOException e){
+            System.out.println("An error occured while writing to the file.");
+            e.printStackTrace();
+        }
+    }
 }
